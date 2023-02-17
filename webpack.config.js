6b9d42e9bf5e6a9fc8f2b8649cad6759bd79147d
D4Cheap <87541896+D4Cheapp@users.noreply.github.com>
2023-02-17 12:19:56 +0300

    Initial commit
    Создана база для проекта. React, webpack, sass, typescript, postcss

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports={
+    //Режим проекта и точка входа
+    mode: 'development',
+    entry: path.resolve(__dirname,'./src/index.tsx'),
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    stats: {
+        children: true,
+    },
+    //Настройки сервера
+    devServer: {
+        watchFiles: [path.resolve(__dirname,"src")],
+        historyApiFallback: true,
+        port:3000,
+        hot:true,
+    },
+    //Выходной main файл
+    output: {
+        filename: `[name].js`,
+        path: path.resolve(__dirname,'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader",'css-loader']
+            },
+            {
+                //Компиляция из sass в css
+                test: /\.sass$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', {
+                    loader: 'sass-loader',
+                    options: {
+                        //Файл с переменными и миксинами
+                        additionalData: '@import "src/sass/_variablesAndGlobal.sass"',
+                    }},]
+            },
+            {
+                //Обработка картинок, иконок, svg
+                test: /\.(jpg|png|svg|ico)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path]/[name].[ext]'
+                    }},
+                    {
+                        // Оптимизация всех изображений
+                        loader: "webp-loader",
+                        options: {
+                            quality: 75
+                        }
+                    }
+            ]}],
+    },
+    plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname,'./src/index.html')}),
+        new CleanWebpackPlugin()]
+}
